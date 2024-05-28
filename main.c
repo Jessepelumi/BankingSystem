cd909e1854d067b74841ed6de93cdad8651821d1
Adesina Jesse <jesseoluwapelumi01@gmail.com>
2024-05-28 09:29:15 +0100

    created a structure for the account

--- main.c
+++ main.c
@@ -1,8 +1,48 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+struct Account
+{
+    char account_name[30];
+    char account_number[10];
+    char account_password[24];
+    int transaction_pin;
+    float account_balance;
+    float loan_balance;
+};
 
 int main()
 {
-    printf("The bank works");
+    struct Account account;
+    int option;
+
+    printf("Welcome to Jeolad Banking System\n\n");
+    printf("Enter the appropriate option to perform the corresponding operation\n");
+
+    printf("Enter your option here: ");
+    scanf("%d", &option);
+    printf("%d\n", option);
+
+    if (option == 1)
+    {
+        system("clear");
+        printf("Account name: ");
+        scanf("%s", account.account_name);
+        printf("Account number: ");
+        scanf("%s", account.account_number);
+        printf("Account password: ");
+        scanf("%s", account.account_password);
+        printf("Transaction pin: ");
+        scanf("%d", &account.transaction_pin);
+        account.account_balance = 0;
+        account.loan_balance = 0;
+    } else if (option == 2)
+    {
+        printf("Login module\n");
+    } else {
+        printf("invalid option\n");
+    }
 
     return 0;
 }
