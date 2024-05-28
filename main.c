6f91dedf0fc921120f8e06d4e2b63d836c3a5c41
Adesina Jesse <jesseoluwapelumi01@gmail.com>
2024-05-28 14:32:10 +0100

    created structure for account creation and login

--- main.c
+++ main.c
@@ -5,8 +5,8 @@
 struct Account
 {
     char account_name[30];
-    char account_number[10];
-    char account_password[24];
+    char account_number[15];
+    char account_password[25];
     int transaction_pin;
     float account_balance;
     float loan_balance;
@@ -15,7 +15,12 @@ struct Account
 int main()
 {
     struct Account account;
+    FILE *file_pointer;
+    char filename[15];
     int option;
+
+    char user_account_number[15];
+    char user_account_password[25];
 
     printf("Welcome to Jeolad Banking System\n\n");
     printf("Enter the appropriate option to perform the corresponding operation\n");
@@ -26,7 +31,12 @@ int main()
 
     if (option == 1)
     {
-        system("clear");
+#ifdef _WIN32
+        system("cls"); // Windows
+#else
+        system("clear"); // Unix-based systems
+#endif
+
         printf("Account name: ");
         scanf("%s", account.account_name);
         printf("Account number: ");
@@ -37,10 +47,65 @@ int main()
         scanf("%d", &account.transaction_pin);
         account.account_balance = 0;
         account.loan_balance = 0;
-    } else if (option == 2)
+
+        strcpy(filename, account.account_number);
+        strcat(filename, ".dat");
+
+        printf("Filename: %s\n", filename);
+        file_pointer = fopen(filename, "wb");
+        size_t written = fwrite(&account, sizeof(struct Account), 1, file_pointer);
+
+        if (written == 1)
+        {
+            printf("Account successfully registered.\n");
+        }
+        else
+        {
+            printf("Error in creating account. Try again.\n");
+        }
+
+        fclose(file_pointer);
+    }
+    else if (option == 2)
     {
-        printf("Login module\n");
-    } else {
+#ifdef _WIN32
+        system("cls"); // Windows
+#else
+        system("clear"); // Unix-based systems
+#endif
+
+        printf("Enter your credentials to login\n");
+        printf("Account number: ");
+        scanf("%s", user_account_number);
+        printf("Password: ");
+        scanf("%s", user_account_password);
+
+        strcpy(filename, user_account_number);
+        strcat(filename, ".dat");
+        file_pointer = fopen(filename, "r");
+        if (file_pointer == NULL)
+        {
+            printf("Account does not exist. Create new account.\n");
+        }
+        else
+        {
+            fread(&account, sizeof(struct Account), 1, file_pointer);
+            fclose(file_pointer);
+
+            int password_check = strcmp(user_account_password, account.account_password);
+
+            if (password_check == 0)
+            {
+                printf("Valid password\n");
+            }
+            else
+            {
+                printf("Invalid password\n");
+            }
+        }
+    }
+    else
+    {
         printf("invalid option\n");
     }
 
